b2e620271a223dd3b275fdfa89cb2206165ac63f
Joshua Boniface <joshua@boniface.me>
2018-04-07 02:24:37 -0400

    Major rewrite to fix several consistency, readability, and safety issues with the original daemon and trigger

--- trigger.py
+++ trigger.py
@@ -4,26 +4,63 @@
 # Copyright 2018 Joshua Boniface <joshua@boniface.me>
 # See LICENSE for licensing details
 
-# Usage:
-#  $ client.py <daemon.py function>
-#
-# Valid colours: white, green, blue, red
-#
-# e.g.
-#  $ client.py leds_off
-#  $ client.py leds_white
-#  $ client.py leds_green
-#  $ client.py leds_held_blue [3 seconds]
-#  $ client.py leds_blink_red [until leds_off]
+def usage():
+    print('trigger.py: Send a command to the respeaker-led daemon')
+    print('')
+    print('Usage:')
+    print('')
+    print('$ trigger.py <action> [<colour>] [<holdtime>]')
+    print('')
+    print('Actions:')
+    print('  off: Turn off LEDs (unless held)')
+    print('  on: Turn on LEDs until next off action')
+    print('  flash: Flash LEDs until next off action')
+    print('  hold: Keep LEDs on for a specific time unless overridden')
+    print('')
+    print('Colours:')
+    print('  white red blue green yellow cyan magenta')
+    print('')
+    print('Holdtime:')
+    print('  The duration in seconds to keep the LEDs on for hold action')
+    print('')
+    print('Examples:')
+    print('  $ trigger.py off')
+    print('  $ trigger.py on red')
+    print('  $ trigger.py hold white 5')
+    print('  $ trigger.py flash cyan')
 
-led_cmd = '/run/led_cmd'
+# Define our socket to talk to the daemon
+cmd_socket = '/run/respeaker-led.sock'
 
-import os
-import sys
+import os, sys
 
-cmd = sys.argv[1]
+# Get the command from the CLI
+try:
+    cmd = ' '.join(sys.argv[1:])
+# Or set it to an empty string
+except:
+    cmd = ''
 
-fcmd = open(led_cmd, 'w')
+# If cmd is an empty string,
+if cmd == '':
+    # Print usage and exit with failure
+    usage()
+    exit(1)
+
+# Try to open the socket writeable
+try:
+    fcmd = open(cmd_socket, 'w')
+# Or exit with failure
+except:
+    print("Failed to open socket!")
+    exit(1)
+
+# Print argument string to socket
 fcmd.write(str(cmd) + '\n')
+
+# Flush and close the socket
 fcmd.flush()
 fcmd.close()
+
+# Exit with success
+exit (0)
